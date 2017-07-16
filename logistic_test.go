9649eca420f350276455b2d8ab52eaf76abe56ac
WindomZ <git.windomz@gmail.com>
2017-07-17 00:55:37 +0800

    test: more testings

--- logistic_test.go
+++ logistic_test.go
@@ -40,3 +40,20 @@ func TestLogisticMap(t *testing.T) {
 
 	assert.Equal(t, avgs, []int{5901, 2752, 2263, 2074, 1895, 1941, 2044, 2163, 2725, 6243})
 }
+
+func TestLogistic_Next(t *testing.T) {
+	const u = 3.9999
+	const x = 0.2
+	const COUNT = 30000
+
+	l := NewLogistic(false, u, x)
+	result1 := make([]float64, COUNT+1)
+	result1[0] = x
+	for i := 1; i <= COUNT; i++ {
+		result1[i] = l.Next().Value()
+	}
+
+	result2 := LogisticMap(u, x, COUNT)
+
+	assert.Equal(t, result1, result2)
+}
